08b122f1d561e18f10aa2aab8d6f6941d074d6aa
berlinevan <97455100+berlinevan@users.noreply.github.com>
2024-02-20 11:44:07 -0500

    Made Start Menu
    Added a new scene with menu buttons to start the game and exit the program.  Nothing is linked to the options button that was also added.

---
+++ EverBloom/Start Menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://World/world.tscn")
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+func _on_exit_pressed():
+	get_tree().quit()
