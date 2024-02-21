ef928e5d4c976597d31c47aece9f86c54ac81e1d
PineGragg <148920500+PineGragg@users.noreply.github.com>
2024-02-21 15:04:37 -0500

    fixed the options menu going to the start screen from being paused

--- EverBloom/Pause Menu.gd
+++ EverBloom/Pause Menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 func _on_resume_button_pressed():
+	get_tree().paused = not get_tree().paused
 	get_tree().change_scene_to_file("res://World/world.tscn")
 	
 func _on_inventory_button_pressed():
@@ -12,3 +13,8 @@ func _on_options_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _input(event):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = not get_tree().paused
+		get_tree().change_scene_to_file("res://World/world.tscn")
