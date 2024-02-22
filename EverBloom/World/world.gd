05c9266af5ffbbef76a58c64f0f5fcd6b3195651
Jude2Tuff <judedenton22@gmail.com>
2024-02-22 15:55:38 -0500

    Added the ability to till Grass
    Tills the grass tile in front of your into dirt.

--- EverBloom/World/world.gd
+++ EverBloom/World/world.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const plant = preload("res://Plants/Plant.tscn")
 @onready var world = $"."
+@onready var player = $player
+@onready var floor = $Floor
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
@@ -13,4 +15,19 @@ func _input(event):
 	if Input.is_action_just_pressed("pause"):
 		get_tree().paused = not get_tree().paused
 		get_tree().change_scene_to_file("res://Pause Menu.tscn")
-		
+	
+	if event.is_action("Till"):
+		var playerLocal = player.position
+		var tilePositon = floor.local_to_map(playerLocal)
+		var direction = player.getLastInput()
+		match direction:
+			"ui_right":
+				tilePositon.x += 1
+			"ui_left":
+				tilePositon.x -= 1
+			"ui_down":
+				tilePositon.y += 1
+			"ui_up":
+				tilePositon.y -= 1
+		print(tilePositon)
+		floor.set_cell(0, tilePositon, 4, Vector2i(1,1))
