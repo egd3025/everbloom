6453b58cf2067a40f8fd2cd4086781beb188ec9b
Jude2Tuff <judedenton22@gmail.com>
2024-03-13 17:38:38 -0400

    Revert "Merge pull request #34 from UNCW-CSC-450/revert-33-Ysorting"
    This reverts commit 1820e5aa7520e4555cc748ff57a142c62ac9e2ad, reversing
    changes made to 9f3ae1c69c85f345e30d4cd977b703518f265a86.

--- EverBloom/Pause Menu.gd
+++ EverBloom/Pause Menu.gd
@@ -6,8 +6,7 @@ func _on_resume_button_pressed():
 	get_tree().change_scene_to_file("res://World/world.tscn")
 	
 func _on_inventory_button_pressed():
-	get_tree().paused = not get_tree().paused
-	get_tree().change_scene_to_file("res://Inventory/inventory_ui.tscn")
+	pass
 	
 func _on_options_button_pressed():
 	get_tree().change_scene_to_file("res://options_menu.tscn")
@@ -16,6 +15,6 @@ func _on_quit_button_pressed():
 	get_tree().quit()
 
 func _input(event):
-	if event.is_action_pressed("pause"):
+	if Input.is_action_just_pressed("pause"):
 		get_tree().paused = not get_tree().paused
 		get_tree().change_scene_to_file("res://World/world.tscn")
--- EverBloom/World/WorldGen.gd
+++ EverBloom/World/WorldGen.gd
@@ -1,19 +1,21 @@
 extends TileMap
 
 @onready var floor = $"."
-
-
+var  x = 50
+var  y = 50
+var coordArray = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	place_tiles()
 
 
+
 func place_tiles():
-	var  x = 150
-	var  y = 150
+	
 	
 	for row in range(x):
 		for col in range(y):
+			coordArray.append(Vector2i(x,y))
 			var random_number : int = randi() % 50
 			if 0 <= random_number and random_number < 25:
 				floor.set_cell(0, Vector2i(row,col), 2, Vector2i(1,1))
@@ -23,4 +25,18 @@ func place_tiles():
 				floor.set_cell(0, Vector2i(row,col), 2, Vector2i(0,6))
 			if random_number >= 37 and random_number <= 50:
 				floor.set_cell(0, Vector2i(row,col), 2, Vector2i(0,5))
+		#floor.set_cells_terrain_connect(0, [Vector2i(x,y)], 0, 0)
 
+	for row in range(x):
+		for col in range(y):
+			var random_number : int = randi() % 50
+			if random_number == 25:
+				floor.set_cell(2, Vector2i(row,col), 5, Vector2i(0,0))
+			
+			if random_number == 0:
+				floor.set_cell(2, Vector2i(row,col), 5, Vector2i(1,0))
+
+
+func getCoordArray():
+	print(coordArray)
+	return coordArray
