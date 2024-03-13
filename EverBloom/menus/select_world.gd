1cd38d08cc35b2fda3a1a98cf4b35faf2b9691b5
berlinevan <97455100+berlinevan@users.noreply.github.com>
2024-03-13 10:17:51 -0400

    added a world select menu
    moved the menus to their own folder as well as added a select world scene

---
+++ EverBloom/menus/select_world.gd
@@ -0,0 +1,15 @@
+extends Control
+
+#select menu
+
+
+func _on_start_world_pressed():
+	get_tree().change_scene_to_file("res://World/world.tscn")
+
+
+func _on_new_world_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://menus/start_menu.tscn")
