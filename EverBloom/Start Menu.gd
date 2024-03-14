c715bdd67abf12835434f78f965a9a7efa1119c3
Jude2Tuff <judedenton22@gmail.com>
2024-03-13 21:43:20 -0400

    Revert "Revert "Revert "Merge pull request #36 from UNCW-CSC-450/save_game"""
    This reverts commit efba505ce67bef2bd866b65866b002ab7c11dabe.

--- EverBloom/Start Menu.gd
+++ EverBloom/Start Menu.gd
@@ -1,8 +1,7 @@
 extends Control
-#start menu
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://menus/select_world.tscn")
+	get_tree().change_scene_to_file("res://World/world.tscn")
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://options_menu.tscn")
