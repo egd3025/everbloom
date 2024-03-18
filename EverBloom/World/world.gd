07a81f69ad8848e75e932c8763bf57b38f4a8c46
Jude2Tuff <judedenton22@gmail.com>
2024-03-17 20:48:18 -0400

    tilling event made
    Added the basis of tilling

--- EverBloom/World/world.gd
+++ EverBloom/World/world.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const plant = preload("res://Plants/Plant.tscn")
 @onready var world = $"."
+@onready var floor = $Floor
+@onready var player = $player
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
@@ -17,4 +19,15 @@ func _input(event):
 	if Input.is_action_just_pressed("inventory"):
 		get_tree().change_scene_to_file("res://Inventory/inventory_ui.tscn")
 		
+	if event.is_action("till"):
+		var coord = to_local(Vector2(player.position.x,player.position.y))
+		coord = floor.local_to_map(coord)
+		var coordList = []
+		for x in range(-1, 2):
+			for y in range(-1,2):
+				coordList.append(Vector2i(player.position.x + x,player.position.y + y))
+		floor.set_cell(1, coord, 3, Vector2(1,1))
+		floor.set_cells_terrain_connect(1, coordList, 0, 0)
+
 		
+	
