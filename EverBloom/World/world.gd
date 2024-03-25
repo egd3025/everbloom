29a2bc73e74b2dc1cab336863a50fb14f038fc94
PineGragg <148920500+PineGragg@users.noreply.github.com>
2024-03-24 21:33:12 -0400

    fixed all the inventory bugs, got mostly functionality with the hotbar, stuggling to get it to display in the world
    fixed inventory not closing, double planting, and collect functions

--- EverBloom/World/world.gd
+++ EverBloom/World/world.gd
@@ -7,17 +7,10 @@ const plant = preload("res://Plants/Plant.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
-	if event.is_action("Spawn"):
-		var plant1 = plant.instantiate()
-		plant1.position = get_global_mouse_position()
-		world.add_child(plant1)
 		
-	if Input.is_action_just_pressed("pause"):
+	if event.is_action("pause"):
 		get_tree().paused = not get_tree().paused
 		get_tree().change_scene_to_file("res://Pause Menu.tscn")
-		
-	if Input.is_action_just_pressed("inventory"):
-		get_tree().change_scene_to_file("res://Inventory/inventory_ui.tscn")
 		
 	if event.is_action("till"):
 		var coord = to_local(Vector2(player.position.x,player.position.y))
@@ -27,5 +20,18 @@ func _input(event):
 			floor.set_cell(2, coord, 6, Vector2(1,1))
 			floor.set_cells_terrain_connect(2, [coord], 0, 0)
 
+func _process(delta):
+	# inventory was staying open using the input(event) methods, this
+	# allows for closing the inventory by pressing i again
+	if Input.is_action_just_pressed("inventory"):
+		get_tree().change_scene_to_file("res://Inventory/inventory_ui.tscn")
 		
+	# having the planting method in the process function removes the bug of
+	# double planting, idk WHY but it does
+	# if there is no texture appearing after collecting check that in the plant 
+	# scene, the exported Item variable has a .tres file in it this took me SO LONG to figure out :((((
+	if Input.is_action_just_pressed("Spawn"):
+		var plant1 = plant.instantiate()
+		plant1.position = get_global_mouse_position()
+		world.add_child(plant1)
 	
