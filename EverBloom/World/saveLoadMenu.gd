f1a8d5785a6bc41368d4387a7deea917b6034972
berlinevan <97455100+berlinevan@users.noreply.github.com>
2024-03-26 22:20:22 -0400

    changed a few script settings

--- EverBloom/World/saveLoadMenu.gd
+++ EverBloom/World/saveLoadMenu.gd
@@ -1,9 +1,16 @@
 extends VBoxContainer
 
+@export var game_manager : GameManager
 
+func _on_game_manager_toggle_game_paused(is_paused : bool):
+	if(is_paused):
+		show()
+	else:
+		hide()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
+	game_manager.connect("toggel_game_pause", _on_game_manager_toggle_game_paused)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
