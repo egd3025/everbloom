110a4b07b0f4d5e652140d384519a73546179b2c
PineGragg <148920500+PineGragg@users.noreply.github.com>
2024-03-27 15:35:49 -0400

    testing a stacking issue solution

--- EverBloom/Inventory/inventory.gd
+++ EverBloom/Inventory/inventory.gd
@@ -7,14 +7,20 @@ signal update
 @export var slots: Array[InvSlot]
 
 func insert(item: InvItem):
+	var isAdded = false;
 	var itemslots = slots.filter(func(slot): return slot.item == item)
 	# is this item already in the inventory? add it to the rest
 	if !itemslots.is_empty():
-		itemslots[0].amount += 1
-	else:
+		# check if theres already a max stack of this item
+		if itemslots[0].amount != 99:
+			# there's not, add it to the stack
+			itemslots[0].amount += 1
+			isAdded = true;
+	if not isAdded:
 		var emptyslots = slots.filter(func(slot): return slot.item == null)
 		# item is not in the inventory already, goes into a new slot
 		if !emptyslots.is_empty():
 			emptyslots[0].item = item
 			emptyslots[0].amount = 1
+			isAdded = true;
 	update.emit()
