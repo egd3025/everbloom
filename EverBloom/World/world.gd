90348ebd50263f5c0c8f9f80c4034f2cf1342f59
Jude2Tuff <judedenton22@gmail.com>
2024-04-09 14:25:50 -0400

    added comments in world.gd

--- EverBloom/World/world.gd
+++ EverBloom/World/world.gd
@@ -4,34 +4,42 @@ const plant = preload("res://Plants/Plant.tscn")
 @onready var world = $"."
 @onready var floor = $Floor
 @onready var player = $player
+#coordinate list for all the plants 
 var coordList = []
 # makes a variable to recieve the signal from hotbar
 var hotbarSelected = 2;
 
 func _input(event):
 	if event.is_action("Plant"):
+		#flag to see if there is already plant on that tile
 		var flag = true
+		#gets the players position on the tile map
 		var coord = to_local(Vector2(player.position.x,player.position.y))
 		coord = floor.local_to_map(coord)
-		
+		#checks to see if there is a plant whos coordinates are that tile
 		for plantCoord in coordList:
 			if plantCoord[0] == coord:
 				flag = false
-	
+		#if there isnt a plant and the tile is tilled dirt
 		if floor.get_cell_atlas_coords(2, coord) == Vector2i(1,1) and flag:
 			coord = floor.map_to_local(coord)
+			#create a plant instance
 			var plant1 = plant.instantiate()
+			#set coords
 			plant1.position = coord
+			#add it as a child of the world and at it to the group "plants" for saving
 			world.add_child(plant1)
 			plant1.add_to_group("Plants")
 			coordList.append([floor.local_to_map(coord), plant1])
-			print(coordList)
 
 	if event.is_action_pressed("collect"):
+		#get the players coordinates
 		var coord = to_local(Vector2(player.position.x,player.position.y))
 		coord = floor.local_to_map(coord)
+		#if the player is standing on a plant
 		for x in range(len(coordList)):
 			if coordList[x][0] == coord:
+				#if its harvestable remove it from the game and remove it at the list and break
 				if coordList[x][1].stage > 4:
 					coordList[x][1].queue_free()
 					coordList.remove_at(x)
@@ -54,6 +62,7 @@ func _input(event):
 			var coord = to_local(Vector2(player.position.x,player.position.y))
 			coord = floor.local_to_map(coord)
 			var coordList = []
+			#change the tile in the tile map and call the autotile operation
 			if floor.get_cell_source_id(1, coord) != 0:
 				floor.set_cell(2, coord, 6, Vector2(1,1))
 				floor.set_cells_terrain_connect(2, [coord], 0, 0)
