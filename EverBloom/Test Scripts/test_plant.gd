be30a85cd2024630e30f5f56bf830f949dc7b434
Jude2Tuff <judedenton22@gmail.com>
2024-04-11 18:20:42 -0400

    Installed GUT
    installed gut and created the first test

---
+++ EverBloom/Test Scripts/test_plant.gd
@@ -0,0 +1,9 @@
+extends GutTest
+@onready var plant = $World/Plant
+
+func test_plant_instantiate():
+	var plant1 = plant.instantiate()
+	
+	assert_eq(plant1.stage, 1, "Plant stage when created is 1")
+	
+	
