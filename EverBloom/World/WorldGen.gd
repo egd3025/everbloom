3230d0eec6f441d669601778729f2826184b1e82
Jude2Tuff <judedenton22@gmail.com>
2024-04-11 14:32:17 -0400

    world gen fix
    adds more world gen and world details
    fixed the dimensions of the loops so trees are not being places on things they should not be placed on

--- EverBloom/World/WorldGen.gd
+++ EverBloom/World/WorldGen.gd
@@ -29,11 +29,21 @@ func place_tiles():
 			if random_number >= 37 and random_number <= 50:
 				floor.set_cell(0, Vector2i(row,col), 2, Vector2i(0,5))
 	#loops for trees
-	for row in range(-20,x+1):
-		for col in range(-25,y+1):
-			var random_number : int = randi() % 50
+	for row in range(-2, 20):
+		for col in range(1, 11):
+			var random_number : int = randi() % 60
 			if random_number == 49:
 				floor.set_cell(1, Vector2i(row,col), 0, Vector2i(1,0))
 			if random_number == 48:
 				floor.set_cell(1, Vector2i(row,col), 0, Vector2i(0,0))
+			if random_number == 47:
+				floor.set_cell(1, Vector2i(row,col), 0, Vector2i(6,3))
+			if random_number == 46:
+				floor.set_cell(1, Vector2i(row,col), 0, Vector2i(6,2))
+			if random_number == 45:
+				floor.set_cell(1, Vector2i(row,col), 0, Vector2i(6,0))
+			if random_number == 44:
+				floor.set_cell(1, Vector2i(row,col), 0, Vector2i(5,0))
+			if random_number == 43:
+				floor.set_cell(1, Vector2i(row,col), 0, Vector2i(6,1))
 
