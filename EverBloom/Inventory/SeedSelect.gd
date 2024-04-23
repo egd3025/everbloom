c90057848b2abe21e86d500c5c3763759476c7e9
PineGragg <148920500+PineGragg@users.noreply.github.com>
2024-04-23 14:00:10 -0400

    changed the almanac texture, simplified the hotbar update for seed select

--- EverBloom/Inventory/SeedSelect.gd
+++ EverBloom/Inventory/SeedSelect.gd
@@ -1,19 +1,18 @@
 extends TextureRect
 
 @onready var tomButton = $Tomato_Button;
-signal Wheat
-signal Tomato
+@onready var hotbar = $"../Hotbar"
 
 func _on_wheat_button_pressed():
 	# Switches the hotbar slot 4 with the wheat and allows the player
 	# to plant wheat seeds
 	# Also tracks how many wheat has been collected and allows for 
 	# tomatoes to be unlocked once the quota has been reached
-	Wheat.emit();
+	hotbar._on_seed_select_wheat()
 
 
 func _on_tomato_button_pressed():
 	# switches the hotbar slot 4 with the tomato and allows the player
 	# to plant tomato seeds once they are unlocked
 	if tomButton.visible:
-		Tomato.emit();
+		hotbar._on_seed_select_tomato()
