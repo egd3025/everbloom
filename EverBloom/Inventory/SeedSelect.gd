8f71d14401029725d5cb8b1fce04d67a5d2382a7
PineGragg <148920500+PineGragg@users.noreply.github.com>
2024-04-28 18:47:07 -0400

    removed tomatoes. now there is an "objective" to "unlock" them
    clicking on the seeds in the inventory reveals what must be done to receive tomatoes. since godot still hates me this isn't actually implemented. and is just a facade for now until i can put legos in their shoes.

--- EverBloom/Inventory/SeedSelect.gd
+++ EverBloom/Inventory/SeedSelect.gd
@@ -2,6 +2,10 @@ extends TextureRect
 
 @onready var tomButton = $Tomato_Button;
 @onready var hotbar = $"../Hotbar"
+@onready var wheatButton = $Wheat_Button;
+@onready var wheatLabel = $wheat_Label;
+@onready var tomLabel = $Tomato_Label;
+@onready var seedLabel = $Label;
 
 func _on_wheat_button_pressed():
 	# Switches the hotbar slot 4 with the wheat and allows the player
@@ -14,5 +18,12 @@ func _on_wheat_button_pressed():
 func _on_tomato_button_pressed():
 	# switches the hotbar slot 4 with the tomato and allows the player
 	# to plant tomato seeds once they are unlocked
-	if tomButton.visible:
-		hotbar._on_seed_select_tomato()
+	# currently world planting textures are fighting me so this is just going to show
+	# as locked and have a prompt appear on how to unlock it
+	seedLabel.visible = false;
+	wheatButton.visible = false;
+	wheatLabel.visible = false;
+	tomButton.visible = false;
+	tomLabel.visible = false;
+	var tomUnlock = $Tomato_Unlock;
+	tomUnlock.visible = true;
